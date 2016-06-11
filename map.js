8ae0ef23c865bd2119294b169775568fe95baace
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2016-06-11 17:17:23 +0500

    add comments

--- map.js
+++ map.js
@@ -1,43 +1,53 @@
 document.getElementById('map').classList.add('loading');
 mapboxgl.accessToken = 'pk.eyJ1Ijoic3RlcGFua3V6bWluIiwiYSI6Ik1ieW5udm8ifQ.25EOEC2-N92NCWT0Ci9w-Q';
 
+// настройка карты
 var map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/mapbox/light-v8',
-  center: [-74.0059, 40.7127],
-  zoom: 12
+  container: 'map', // идентификатор html куда будет рендериться карта
+  style: 'mapbox://styles/mapbox/light-v8', // стиль карты по умлочанию
+  center: [-74.0059, 40.7127], // начальные координаты карты
+  zoom: 12 // начальный уровень приближения
 });
 
+// настройка данных для маркеров
 var markers = new mapboxgl.GeoJSONSource({
-  data: {},
-  cluster: true,
-  clusterRadius: 40
+  data: {}, // данных пока нет, они загрузяться позже
+  cluster: true, // объединять точки в кластеры
+  clusterRadius: 40 // размер кластера в пикселях
 });
 
+// эта функция будет выполнена после загрузки карты
 map.on('load', () => {
-  map.addSource('markers', markers);
+  map.addSource('markers', markers); // добавляем на карту данные маркеров
+
+  // добавляем на карту слой маркеров
   map.addLayer({
-    id: 'markers',
-    type: 'circle',
-    source: 'markers',
+    id: 'markers', // идентификатор слоя
+    type: 'circle', // тип отображения слоя
+    source: 'markers', // идентификатор данных
     paint: {
-      'circle-radius': 5,
-      'circle-color': '#45AE6C'
+      'circle-radius': 5, // радиус кружка
+      'circle-color': '#45AE6C' // цвет кружка
     }
   });
 
-  setTimeout(render, 1000);
+  setTimeout(render, 1000); // отрисовать попапы через секунду
 });
 
+// загрузка маркеров из файла data.geojson
 fetch('data.geojson')
   .then(response => response.json().then(data => {
-    markers.setData(data);
+    markers.setData(data); // загрузка данных в маркеры
     document.getElementById('map').classList.remove('loading');
   }))
   .catch(error => console.error('Error loading data.geojson', error));
 
+// функция отрисовки содержимого попапа
 var renderFeature = (feature) => {
+  // сокращаем текст до восьми символов
   var title = feature.properties.name.substring(0, 8);
+
+  // отдаём html строчку
   return `
     <div class='container'>
       <img class='photo' src=${feature.properties.photo} />
@@ -47,20 +57,29 @@ var renderFeature = (feature) => {
 };
 
 var popups = [];
+
+// функция отрисовки попапов
 var render = () => {
+  // находим все отрисованные объекты на карте в текущем экстенте
   var bounds = map.getBounds();
   var nw = map.project(bounds.getNorthWest());
   var se = map.project(bounds.getSouthEast());
   var bbox = [[nw.x, nw.y], [se.x, se.y]];
   var features = map.queryRenderedFeatures(bbox, { layers: ['markers'] });
 
+  // удаляем все предыдущие маркеры
   popups.map(popup => popup.remove())
+
+  // создаём новые маркеры — из всех найденных объектов выбираем все объекты не-кластеры
   popups = features.filter(feature => !feature.properties.cluster).map(feature => {
+    // создаём для каждого объекты новый попап
     return new mapboxgl.Popup({ closeButton: false, closeOnClick: false, anchor: 'bottom' })
+      // и добавляем его на карту
       .setLngLat(feature.geometry.coordinates)
       .setHTML(renderFeature(feature))
       .addTo(map)
   });
 }
 
+// перерисовывать попапы после каждого движения карты
 map.on('moveend', render);
